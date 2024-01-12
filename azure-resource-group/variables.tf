8a01c3896022c44ba0f94c904a613dfaacfba7bf
unknown <sysadmin@jumphost>
2024-01-12 17:49:04 +0530

    Added resourece-group module

---
+++ azure-resource-group/variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {
+  description = "The name of the Azure resource group."
+  default = "dp1"
+}
+
+variable "location" {
+  description = "The Azure region where the resource group will be created."
+}
